dd96270b4b13bb15d57d98573bdd118a7d560990
Arshit Gajera <arshitgajera24@gmail.com>
2025-10-01 13:43:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Navbar } from './components/navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import {Navbar} from "./components/Navbar/Navbar"
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/Place-Order/PlaceOrder'
